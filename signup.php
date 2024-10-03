03ad8942d843021b0bc76b00e7e325370dc9185d
TriciaTech <131809247+TriciaTech@users.noreply.github.com>
2024-10-03 04:10:27 +0300

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,12 +1,18 @@
 <?php
 include 'db_connect.php'; // Include database connection
 
+// Enable error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
     $sql = "INSERT INTO users (username, password) VALUES ('$username', '$hashed_password')";
+    
     if ($conn->query($sql) === TRUE) {
         echo "Signup successful!";
     } else {
@@ -14,3 +20,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
+
